cb04472f76dcf6a2261655680703100316c065ec
Melroy van den Berg <melroy@melroy.org>
2025-11-26 01:22:17 +0100

    Hide the body logging for now again. I have seen enough

--- src/routes/telegram.js
+++ src/routes/telegram.js
@@ -1,12 +1,12 @@
 import 'dotenv/config'
 import { Router } from 'express'
-import logger from '../logger.js'
+//import logger from '../logger.js'
 const router = Router()
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
 
 // We are receiving updates at the route below
 router.post(`/bot${TELEGRAM_TOKEN}`, (req, res) => {
-  logger.error(req.body, 'Received Telegram update with body')
+  //logger.error(req.body, 'Received Telegram update with body')
   // Check if undefined (in express v5 body can be undefined)
   if (req.body) {
     req.telegram_bot.processUpdate(req.body)
